02fc0ad188266a93ec0970979362ff0c79096f34
Gabriel Nascimento <57226652+gabirlSilva@users.noreply.github.com>
2021-04-12 08:58:35 -0300

    Add files via upload

---
+++ ConversorCSV/ConversorCSV/Model/Processo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorCSV
+{
+    class Processo
+    {
+        public long idDocumento { get; set; }
+        public string textoDocumento { get; set; }
+    }
+}
